47fc7832a84e82e0e9dfd62b98fd3adb8c31bcde
owenbmcc <oroberts.bmcc@gmail.com>
2023-05-04 17:28:26 -0400

    testing morgun's scene

--- Scripts/UI.gd
+++ Scripts/UI.gd
@@ -12,3 +12,11 @@ func _on_Back_to_Menu_pressed():
 
 func _on_Quit_pressed():
 	get_tree().quit()
+
+
+func _on_MorgunButton_pressed():
+	get_tree().change_scene("res://Scenes/MWgame/mw_vrscene.tscn")
+
+
+func _on_TestOwenScene_pressed():
+	get_tree().change_scene("res://Scenes/MWgame/mw_test2.tscn")
